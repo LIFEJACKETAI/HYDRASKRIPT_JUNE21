f556ce3d17745ff2bf7ac196aa65f8f6828fe8e7
c-jay69 <simonjc.mex@gmail.com>
2026-08-04 08:38:15 -0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer && Array.isArray(config.externals)) {
+      config.externals.push('pdfkit');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
